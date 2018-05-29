76510a19fa40aa5c500b06aadbdb2a66d490d6c2
petek77 <39743982+petek77@users.noreply.github.com>
2018-05-29 22:18:38 +0300

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,8 +1,8 @@
 //GENERAL HOSTS
-var _HOST_DOMOTICZ		= 'http://user:pass@192.168.1.3:8084'; //IP-address of Domoticz-server, e.g.: http://192.168.1.3:8084
-var _HOST_NZBGET		= 'http://192.168.1.3:6789/jsonrpc';
+var _HOST_DOMOTICZ		= 'http://user:pass@192.168.0.110:8084'; //IP-address of Domoticz-server, e.g.: http://192.168.1.3:8084
+var _HOST_NZBGET		= 'http://192.168.0.110:6789/jsonrpc';
 
 //HOSTS IF DASHTICZ IS INSTALLED ON A WEBSERVER WITH PHP-SUPPORT
-var _HOST_XBMC			= 'http://192.168.1.109:8080'; //IP-address of XBMC/Kodi device, e.g.: http://192.168.1.109:8080
+var _HOST_XBMC			= '';//http://192.168.1.109:8080'; //IP-address of XBMC/Kodi device, e.g.: http://192.168.1.109:8080
 var _HOST_JOINTSPACE	= '';//http://192.168.1.51:1925'; //IP-address for televisions supported by JointSpace, e.g.: http://192.168.1.51:1925
 var _HOST_PLEX			= '';//http://192.168.1.28:32400';
